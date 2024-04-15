44a6d2d1e204edffc7fefc39756ebf7873cd64c9
Jang Min Seon <zzzodg@gmail.com>
2024-04-15 15:00:40 +0900

    소나무 신규홈페이지 소스 커밋

---
+++ html/bbs/_head3.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/css/swiper-bundle.min.css">
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
+  <link rel="stylesheet" href="/css/font.css">
+  <link rel="stylesheet" href="/css/hover.css">
+  <link rel="stylesheet" href="/css/animate.css">
+  <link rel="stylesheet" href="/css/common.css">
+  <link rel="stylesheet" href="/css/layout.css">
+  <link rel="stylesheet" href="/css/media.css">
+  <link rel="stylesheet" href="/css/sub.css">
+  <script src="/js/jquery-3.7.1.min.js"></script>
+  <script src="/js/swiper-bundle.min.js"></script>
+  <script src="/js/wow.min.js"></script>
+  <script src="/js/main.js"></script>
+  <title>주식회사 소나무정보기술</title>
+</head>
+<body>
+  <div class="wrap">
+    <!--헤더-->
+    <header id="header">
+      <div class="header_inner">
+        <h1 class="header_logo">
+          <a href="/index.html">
+            <img src="/images/common/img_logo.png" alt="주식회사 소나무정보기술">
+          </a>
+        </h1>
+        <div class="right_area">
+          <nav id="navi">
+            <ul class="gnb">
+              <li>
+                <a href="/page/sub01_01.html">회사소개</a>
+                <ol class="depth2">
+                  <li>
+                    <a href="/page/sub01_01.html">
+                      연혁
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/page/sub01_02.html">
+                      인증 및 자격
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/bbs/board.php?bo_table=news">
+                      보도자료
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/page/sub01_04.html">
+                      오시는 길
+                    </a>
+                  </li>
+                </ol>
+              </li>
+              <li>
+                <a href="/page/sub02_01.html">사업분야</a>
+                <ol class="depth2">
+                  <li>
+                    <a href="/page/sub02_01.html">
+                      솔루션 개발
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/page/sub02_02.html">
+                      기업 사이트 구축
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/page/sub02_03.html">
+                      구축사례
+                    </a>
+                  </li>
+                </ol>
+              </li>
+              <li>
+                <a href="/bbs/board.php?bo_table=notice">고객센터</a>
+                <ol class="depth2">
+                  <li>
+                    <a href="/bbs/board.php?bo_table=notice">
+                      공지사항
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/member/login.html">
+                      고객지원
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/bbs/board.php?bo_table=file">
+                      자료실
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/bbs/board.php?bo_table=gallery">
+                      소식
+                    </a>
+                  </li>
+                </ol>
+              </li>
+            </ul>
+          </nav>
+          <button type="button" class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+      </div>
+    </header>
+    <!--햄버거 메뉴-->
+    <div class="popup_menu" style="display:none;">
+      <ul class="popup_depth1">
+        <li>
+          <a href="/page/sub01_01.html">
+            회사소개
+          </a>
+          <ul class="popup_depth2">
+            <li>
+              <a href="/page/sub01_01.html">연혁</a>
+            </li>
+            <li>
+              <a href="/page/sub01_02.html">인증 및 자격</a>
+            </li>
+            <li>
+              <a href="/bbs/board.php?bo_table=news">보도자료</a>
+            </li>
+            <li>
+              <a href="/page/sub01_04.html">오시는 길</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="/page/sub02_01.html">
+            사업분야
+          </a>
+          <ul class="popup_depth2">
+            <li>
+              <a href="/page/sub02_01.html">솔루션 개발</a>
+            </li>
+            <li>
+              <a href="/page/sub02_02.html">기업 사이트 구축</a>
+            </li>
+            <li>
+              <a href="/page/sub02_03.html">구축사례</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="/bbs/board.php?bo_table=notice">
+            고객센터
+          </a>
+          <ul class="popup_depth2">
+            <li>
+              <a href="/bbs/board.php?bo_table=notice">공지사항</a>
+            </li>
+            <li>
+              <a href="/member/login.html">고객지원</a>
+            </li>
+            <li>
+              <a href="/bbs/board.php?bo_table=file">자료실</a>
+            </li>
+            <li>
+              <a href="/bbs/board.php?bo_table=gallery">소식</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <button type="button" class="btn_close hvr-grow">
+        <i class="xi-close"></i>
+      </button>
+    </div>
+    <!--메인-->
+    <main id="sub_container">
+      <!--서브 공통-->
+      <div class="sub_top">
+        <div class="sub_top_bg sub_top_bg03"></div>
+        <div class="sub_top_txt">
+          <div class="page_location">
+            <i class="xi-home-o"></i>
+            <span>
+              고객센터
+            </span>
+            <span>
+              자료실
+            </span>
+          </div>
+          <h2>
+            Gallery
+          </h2>
+        </div>
+        <ul class="page_depth">
+          <li>
+            <a href="/bbs/board.php?bo_table=notice">
+              공지사항
+            </a>
+          </li>
+          <li>
+            <a href="/member/login.html">
+              고객지원
+            </a>
+          </li>
+          <li class="on">
+            <a href="/bbs/board.php?bo_table=file">
+              자료실
+            </a>
+          </li>
+          <li>
+            <a href="/bbs/board.php?bo_table=gallery">
+              소식
+            </a>
+          </li>
+        </ul>
+        <!-- 모바일 버전 -->
+        <div class="mo_page_depth">
+          <span>
+            자료실
+          </span>          
+          <ul class="page_depth2">
+            <li>
+              <a href="/bbs/board.php?bo_table=notice">
+                공지사항
+              </a>
+            </li>
+            <li>             
+              <a href="/member/login.html">
+                고객지원
+              </a>
+            </li>
+            <li>              
+              <a href="/bbs/board.php?bo_table=file">
+                자료실
+              </a>
+            </li>
+            <li>   
+              <a href="/bbs/board.php?bo_table=gallery">
+                소식
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="sub_container_inner">
+        <h3>자료실</h3>
---
+++ html/bbs/_tail1.php
@@ -0,0 +1,76 @@
+</div>
+</main>
+    <!--푸터-->
+    <footer id="footer">
+      <div class="footer_inner">
+        <h1>
+          <a href="/index.html">
+            <img src="/images/common/img_logo.png" alt="주식회사 소나무정보기술">
+          </a>
+        </h1>
+        <div class="footer_right">
+          <ul class="address_kor">
+            <li>
+              <b>회사명</b>
+              <span>주식회사 소나무정보기술</span>
+            </li>
+            <li>
+              <b>대표자</b>
+              <span>최재영</span>
+            </li>
+            <li>
+              <b>사업자등록번호</b>
+              <span>735-86-01813</span>
+            </li>
+            <li>
+              <b>개발센터</b>
+              <span>부산광역시 사상구 대동로 303 부산디지털밸리 1203호</span>
+            </li>
+            <li>
+              <b>연락처</b>
+              <span>(051) 714-0034</span>
+            </li>
+            <li>
+              <b>본사</b>
+              <span>부산광역시 부산진구 엄광로 176 331호 (동의대학교 산학협력단)</span>
+            </li>
+          </ul>
+          <ul class="address_eng">
+            <li>
+              <b>Name</b>
+              <span>Pine Information Technology Co., Ltd</span>
+            </li>
+            <li>
+              <b>CEO</b>
+              <span>Choi Jaeyeong</span>
+            </li>
+            <li>
+              <b>EID</b>
+              <span>735-86-01813</span>
+            </li>
+            <li>
+              <b>Address (Lab)</b>
+              <span>1203-ho, 303, Daedong-ro, Sasang-gu, Busan</span>
+            </li>
+            <li>
+              <b>Tel.</b>
+              <span>(051) 714-0034</span>
+            </li>
+            <li>
+              <b>Head Office</b>
+              <span>331-ho, 176, Eomgwang-ro, Busanjin-gu, Busan</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+    <!--원격&탑버튼-->
+    <div class="quick_menu_btn">
+      <a href="/download/client.exe" class="remote_btn">
+        원격지원
+      </a>
+      <a href="#" class="top_btn"></a>
+    </div>
+  </div>
+</body>
+</html>
